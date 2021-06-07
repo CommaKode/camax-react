82f2b9b43793e2fc43c41529c500bd7af827d51b
abolfazl <soltani.abolfazl78@gmail.com>
2021-06-08 04:19:12 +0430

    menu toggle color adde

--- src/components/nav-bar/NavMobile.js
+++ src/components/nav-bar/NavMobile.js
@@ -4,19 +4,26 @@ import MobileTopNav from './MobileTopNav';
 
 const NavMobile = () => {
   const [menu, setMenu] = useState('list-off');
+  const [bg, setBg] = useState('bg-white');
+  const [fill, setFill] = useState('svg-fill-black');
+
   const toggleMenu = () => {
     switch (menu) {
       case 'list-off':
         setMenu('');
+        setBg('bg-black');
+        setFill('svg-fill-white');
         break;
       default:
         setMenu('list-off');
+        setBg('bg-white');
+        setFill('svg-fill-black');
         break;
     }
   };
   return (
     <React.Fragment>
-      <MobileTopNav btn={toggleMenu} />
+      <MobileTopNav fill={fill} bg={bg} btn={toggleMenu} />
       <MobileNavLinks menu={menu} />
     </React.Fragment>
   );
