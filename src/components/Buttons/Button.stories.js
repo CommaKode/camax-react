e925c6e01e6ed363f0f9fc51c1fd398c3575c3f1
abolfazl <soltani.abolfazl78@gmail.com>
2021-06-27 22:57:24 +0430

    button component done

--- src/components/Buttons/Button.stories.js
+++ src/components/Buttons/Button.stories.js
@@ -1,4 +1,5 @@
 import Button from './Button';
+import { ReactComponent as ServIcon } from '../../Images/icons/sevices.svg';
 
 export default {
   title: 'Tools/Button',
@@ -10,10 +11,19 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   title: 'Primary',
+  variant: 'btn-primary',
 };
 
-export const secondary = Template.bind({});
-secondary.args = {
-  title: 'secondary',
-  isIcon: true,
+export const Secondary = Template.bind({});
+Secondary.args = {
+  title: 'Secondary',
+  Icon: ServIcon,
+  variant: 'btn-secondary',
+};
+
+export const Card = Template.bind({});
+Card.args = {
+  title: 'Card',
+  Icon: ServIcon,
+  variant: 'btn-card',
 };
