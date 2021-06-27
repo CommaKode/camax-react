179b1dc02819c6f74d18b28b34426e2f4c4d7a91
abolfazl <soltani.abolfazl78@gmail.com>
2021-06-28 00:59:12 +0430

    hero section done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './scss/_reset.scss';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
 // import RouteApp from './RouteApp';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import NavMobile from './components/nav-bar/NavMobile';
@@ -12,6 +13,7 @@ function App() {
         {/* <NavMobile /> */}
         {/* <RouteApp /> */}
         <NavBar />
+        <Hero />
         <Footer />
       </div>
     </Router>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import heroImg from '../../Images/heroPic.png';
 import './Hero.scss';
+import Button from '../Buttons/Button';
+import { ReactComponent as ServIcon } from '../../Images/icons/sevices.svg';
 
 const Hero = () => {
   return (
@@ -9,11 +11,17 @@ const Hero = () => {
         <img className="hero-img" src={heroImg} alt="ایفون تصویری کامکث" />
         <div className="hero-title">
           <span>
-            <h1> ایفون تصویری</h1>
-            <h1>کامکث</h1>
+            <h1 className="doorphone">ایفون تصویری&nbsp;</h1>
+            <h1 className="camax">کامکث</h1>
           </span>
-          <h2>و لوازم روشنایی</h2>
+          <h2 className="lighting">و لوازم روشنایی</h2>
         </div>
+        <Button title="خرید محصول" variant="btn-primary" />
+        <Button
+          title="خدمات پس از فروش"
+          variant="btn-secondary"
+          Icon={ServIcon}
+        />
       </div>
     </div>
   );
