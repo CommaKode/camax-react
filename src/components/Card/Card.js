eab432b9d284d5a3a8e15267c1ae3d94798b0ea0
mehran moalemi <mohammadmoallemi78@gmail.com>
2021-07-21 03:10:55 -0700

    adding cards folders and files

---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Card = props => {
+    const {title , }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+// Card.propTypes = {
+
+// }
+
+export default Card
---
+++ src/components/Card/Card.stories.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+export default {
+    title : 'Cards/Shop Card',
+    // Component:'Card'
+}
+
+
+const Template = (args) =>  <Card {...args}/>
+
