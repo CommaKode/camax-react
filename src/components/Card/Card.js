4169b710bcef5d80117bc40e5e01f87eaeda27bb
mehran moalemi <mohammadmoallemi78@gmail.com>
2021-07-23 10:36:18 -0700

    editting card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,11 +5,10 @@ import { ReactComponent as buyIcon } from '../../Images/icons/buyCart.svg';
 
 const Card = (props) => {
   const { variant, title, price, image, key, ...rest } = props;
-  const url = "../../Images/Shop/"+ image.toUpperCase() +".png";
 
   return (
     <div className={`card ${variant}`} {...rest} key={key}>
-      <img src={url} alt="pic" />
+      <img src={image} alt="pic" />
       <span className="title">{title.toUpperCase()}</span>
       <span className="price">{price} تومان</span>
       <Button variant="btn-card button" title="خرید کالا" Icon={buyIcon} />
