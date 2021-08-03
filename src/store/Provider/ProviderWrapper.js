54a02e41127572b75b6fa8439ce658e5336507d0
mehran moalemi <mohammadmoallemi78@gmail.com>
2021-08-03 04:39:50 -0700

    edit

--- src/store/Provider/ProviderWrapper.js
+++ src/store/Provider/ProviderWrapper.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { Router,Route  } from "react-router";
+import { Router, Route } from "react-router";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 const ProviderWrapper = ({ children, store }) => (
-  <Provider store={store}>
-    {/* <Router>
-      <Route></Route>
-    </Router> */}
-    {children}
-  </Provider>
+  <Provider store={store}>{children}</Provider>
 );
-
-
 
 export default ProviderWrapper;
