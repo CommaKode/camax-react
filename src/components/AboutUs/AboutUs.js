7c13e11a7d0f030423fc93e12ca2eae1e7c02918
abolfazl <soltani.abolfazl78@gmail.com>
2021-08-04 15:46:34 +0430

    info card added

---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './AboutUs.scss';
+
+const AboutUs = () => {
+  return <div className="aboutus-container"></div>;
+};
+
+export default AboutUs;
---
+++ src/components/AboutUs/AboutUs.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AboutUs from './AboutUs';
+
+export default {
+  title: 'PAGES/Home/About Us',
+  component: AboutUs,
+};
+
+const Template = (args) => <AboutUs {...args} />;
+
+export const Primary = Template.bind({});
