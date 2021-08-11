416aca00c5e52c29a8a555e4625cc11d44b6ae10
abolfazl <soltani.abolfazl78@gmail.com>
2021-08-12 01:51:43 +0430

    sorting router

--- src/App.js
+++ src/App.js
@@ -2,32 +2,19 @@ import './scss/_reset.scss';
 import './App.scss';
 import Nav from './components/NavBar/Nav';
 import Footer from './components/Footer/Footer';
-import Hero from './components/Hero/Hero';
 // import RouteApp from './RouteApp';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import NavMobile from './components/nav-bar/NavMobile';
-import ProductPage from './components/Products/ProductPage';
-import HomePageCar from './components/Carousel/HomePageCar';
-import AboutUs from './components/AboutUs/AboutUs';
-import SuggestionCar from './components/Suggestion/SuggestionCar';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import RouteApp from './RouteApp';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/test1">
-          <ProductPage />
-        </Route>
-        <Route exact path="/">
-          <div className="App">
-            <Nav />
-            <Hero cName="hero-line" />
-            <SuggestionCar />
-            <HomePageCar cName="car-line" />
-            <AboutUs cName="aboutus-line" />
-            <Footer />
-          </div>
-        </Route>
+        <div className="App">
+          <Nav />
+          <RouteApp />
+          <Footer />
+        </div>
       </Switch>
     </Router>
   );
--- src/components/Products/ProductPage.js
+++ src/components/Products/ProductPage.js
@@ -1,28 +1,22 @@
-import React from "react";
-import "./Products.scss";
-import Nav from "../NavBar/Nav";
-import ProductGrid from "./ProductGrid";
-import Filters from "../Filter/Filters";
-import Footer from "../Footer/Footer";
+import React from 'react';
+import './Products.scss';
+import ProductGrid from './ProductGrid';
+import Filters from '../Filter/Filters';
 import MonitorItems from '../Detail-Lists/Monitor';
 import HomePageCar from '../Carousel/HomePageCar';
-
-
 
 const ProductPage = (props) => {
   const { variant, ...rest } = props;
   return (
-   <>
-    <div className="product-page">
-      <Nav />
-          <main className="product-main">
+    <>
+      <div className="product-page" {...rest}>
+        <main className="product-main">
           <Filters />
           <ProductGrid panels={MonitorItems} />
-          </main>
-          <HomePageCar />
-        <Footer />
-    </div>
-   </>
+        </main>
+        <HomePageCar />
+      </div>
+    </>
   );
 };
 
