47b3045cfdfd9ee311648f0bb5e8097ca81a9c4a
abolfazl <soltani.abolfazl78@gmail.com>
2021-08-11 14:59:53 +0430

    footer responsive done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,16 @@ import FooterItems from './FooterItems';
 import FooterAddress from './FooterAddress';
 import CopyRight from './CopyRight';
 import logo from '../../Images/icons/logo.svg';
+import FooterDeskList from './FooterDeskList';
 
 const Footer = () => {
   return (
     <footer className={`page-footer`}>
-      <Lists items={FooterItems} cName="container" />
-      <FooterAddress />
+      <Lists items={FooterItems} cName="container footer-list" />
+      <div className="footer-add-list">
+        <FooterAddress />
+        <FooterDeskList items={FooterItems} cName="container" />
+      </div>
       <div className="footer-logo">
         <img src={logo} alt="Camax" />
       </div>
