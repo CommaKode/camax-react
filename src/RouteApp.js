996f6ae8441b2d3e2e476205b41bbab81cb99e96
MehranMoalemi <mohammadmoallemi78@gmail.com>
2021-08-11 22:20:05 -0700

    some changes for name of the components

--- src/RouteApp.js
+++ src/RouteApp.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import ProductPage from './components/Products/ProductPage';
+import ProductsPage from './components/Products/ProductsPage';
 import Home from './Pages/Home';
 
 const RouteApp = () => {
   return (
     <React.Fragment>
-      <Route component={ProductPage} exaxt path="/test1" />
+      <Route component={ProductsPage} exaxt path="/test1" />
       <Route component={Home} exact path="/" />
     </React.Fragment>
   );
--- src/components/Products/ProductsPage.js
+++ src/components/Products/ProductsPage.js
@@ -1,39 +1,18 @@
-<<<<<<< HEAD:src/components/Products/ProductsPage.js
-import React from "react";
-import "./Products.scss";
-import Nav from "../NavBar/Nav";
-import ProductGrid from "./ProductsGrid";
-import Filters from "../Filter/Filters";
-import Footer from "../Footer/Footer";
-import MonitorItems from '../Detail-Lists/Monitor';
-import HomePageCar from '../Carousel/HomePageCar';
-
-
-
-const ProductsPage = (props) => {
-  const { variant, ...rest } = props;
-  return (
-   <>
-    <div className="product-page" {...rest}>
-      <Nav />
-          <main className="product-main">
-=======
 import React from 'react';
 import './Products.scss';
-import ProductGrid from './ProductGrid';
+import ProductsGrid from './ProductsGrid';
 import Filters from '../Filter/Filters';
 import MonitorItems from '../Detail-Lists/Monitor';
 import HomePageCar from '../Carousel/HomePageCar';
 
-const ProductPage = (props) => {
+const ProductsPage = (props) => {
   const { variant, ...rest } = props;
   return (
     <>
       <div className="product-page" {...rest}>
         <main className="product-main">
->>>>>>> 416aca00c5e52c29a8a555e4625cc11d44b6ae10:src/components/Products/ProductPage.js
           <Filters />
-          <ProductGrid panels={MonitorItems} />
+          <ProductsGrid panels={MonitorItems} />
         </main>
         <HomePageCar />
       </div>
