98c89b54925684bbab37cfcf1584230251171176
abolfazl <soltani.abolfazl78@gmail.com>
2021-08-25 00:44:31 +0430

    making navbar sticky

--- src/components/NavBar/Nav.js
+++ src/components/NavBar/Nav.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from './NavBar';
 import DesktopNav from './DesktopNav/DesktopNav';
 
 const Nav = () => {
+  const [scroll, setScroll] = useState('');
+
+  useEffect(() => {
+    let lastScroll = Number;
+
+    window.addEventListener('scroll', () => {
+      const currentScroll = window.pageYOffset;
+
+      if (currentScroll <= 0) {
+        setScroll('');
+      }
+      if (currentScroll > lastScroll && scroll !== 'scroll-down') {
+        setScroll('scroll-down');
+      }
+      if (currentScroll < lastScroll && scroll === 'scroll-down') {
+        setScroll('');
+      }
+
+      lastScroll = currentScroll;
+    });
+  }, [scroll]);
+
   return (
-    <React.Fragment>
+    <header className={`header-sec ${scroll}`}>
       <NavBar />
       <DesktopNav />
-    </React.Fragment>
+    </header>
   );
 };
 
